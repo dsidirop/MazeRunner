f06ad882eb68f627a230161b864f9d2ff290c213
Dominick Sidiropoulos <dominick.sidiropoulos@gmail.com>
2016-07-13 18:04:05 +0300

    - Tweak the tests project (experimental)

--- Code/MazeRunner.Tests/MSTests/UnitTests/MazeFactoryTests/MazeFactoryTests.cs
+++ Code/MazeRunner.Tests/MSTests/UnitTests/MazeFactoryTests/MazeFactoryTests.cs
@@ -83,7 +83,7 @@ namespace MazeRunner.Tests.MSTests.UnitTests.MazeFactoryTests
 
             // Act
             // var action = new Action(() => { new Maze(new Size(0, 10), Point.Empty, new Point(1, 1), new HashSet<Point>()); });
-            var action = new Action(() => { });
+            var action = new Action(delegate { var foo = 1; });
 
             // Assert
             //action.ShouldThrow<ArgumentException>().WithMessage("size");
