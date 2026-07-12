d87a3eb05b414154186ca85544282330635ad06f
Dominick Sidiropoulos <dominick.sidiropoulos@gmail.com>
2026-07-12 19:12:11 +0200

    fix (FormMazeRunnerTester.Designer.cs): update tooltip text for clarity on maze-solving engine performance

--- Code/MazeRunner.TestbedUI/FormMazeRunnerTester.Designer.cs
+++ Code/MazeRunner.TestbedUI/FormMazeRunnerTester.Designer.cs
@@ -428,7 +428,7 @@ namespace MazeRunner.TestbedUI
             lblTip.Name = "lblTip";
             lblTip.Size = new System.Drawing.Size(194, 38);
             lblTip.TabIndex = 2;
-            lblTip.Text = "Note: The engines complete very fast in the background but the UI will take some time to render everything\n\nTip: Press Ctrl + R to reshuffle the current maze";
+            lblTip.Text = "Note: The maze-solving engines manage to complete very fast in the background but the UI will take some time to render everything due to the artificial delay that let's you observe the steps\n\nTip: Press Ctrl + R to reshuffle the current maze";
             lblTip.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // tlpLogs
